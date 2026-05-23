bf2398a14fded336dcec5d159d63188de764c6c4
Sania <saniatiwari05@gmail.com>
2026-05-23 19:52:48 +0530

    update projec

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, jsonify
 import json
+from urllib.parse import unquote
 
 app = Flask(__name__)
 
@@ -7,17 +8,47 @@ app = Flask(__name__)
 with open("festival_data.json", "r", encoding="utf-8") as f:
     festival_data = json.load(f)
 
+
 @app.route("/")
 def home():
     return render_template("index.html")
+
 
 @app.route("/api/festivals")
 def get_festivals():
     return jsonify(festival_data)
 
-@app.route("/api/festival/<name>")
+
+# NORMALIZATION FUNCTION
+def normalize(text):
+
+    return (
+        unquote(text)
+        .lower()
+        .strip()
+        .replace(" ", "")
+        .replace("-", "")
+    )
+
+
+@app.route("/api/festival/<path:name>")
 def get_festival_by_name(name):
+
+    search_name = normalize(name)
+
     for festival in festival_data["festival_intelligence_engine"]:
-        if festival["festival_or_tithi"].lower() == name.lower():
+
+        festival_name = normalize(
+            festival["festival_or_tithi"]
+        )
+
+        if festival_name == search_name:
             return jsonify(festival)
-    return jsonify({"error": "Festival not found"}), 404
+
+    return jsonify({
+        "error": "Festival not found"
+    }), 404
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
